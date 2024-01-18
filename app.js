fdf0caae51457aad08c96383c8e869ce3361d051
Shinvx <156226474+Shinvx@users.noreply.github.com>
2024-01-18 18:12:33 +0100

    init 18.01.2024
    18.01.2024

--- app.js
+++ app.js
@@ -21,15 +21,42 @@ cancelAnimationFrame(headeri);
 
 let xi = document.getElementById("X");
 let yi = document.getElementById("Y");
-let da = document.getElementById("maindrawID")
+let da = document.getElementById("maindrawID");
+
+class MousePosition {
+
+
+  #mousePosXEl;
+  get mousePosX() {
+    return this.#mousePosXEl.value;
+  }
+  set mousePoX(val) {
+    this.#mousePosXEl.value = val;
+    const cev = new CustomEvent("poschanged", { detail: { x: val } });
+    document.body.dispatchEvent(cev);
+  }
+
+  constructor(mousePosxEl) {
+    this.#mousePosXEl = mousePosxEl;
+  }
+}
+
 function coordinate(event) {
   let x = event.clientX ; //270 1023 
   let y = event.clientY ;//150 524
-  xi.value = x;
+  document.body.appState.mousePoX = x;
   yi.value = y;
 }
 
-
+//onmousedown ?
 function pen(e, color) {
-
+  
 }
+
+document.body.appState = new MousePosition(xi);
+
+
+document.body.addEventListener("poschanged", (ev) => {
+
+  console.info("x: "+ev.detail.x);
+});
